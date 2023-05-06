4161b9171b0fad6e8c0d239da2493558f409634c
fraguival <fran@fraguival.com>
2023-05-06 10:33:11 -0700

    add

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const gate = document.querySelector('.gate')
+const menu = document.querySelector('.menu')
+
+gate.addEventListener('click', () => {
+    gate.classList.toggle('gyre')
+    menu.classList.toggle('look')
+})
